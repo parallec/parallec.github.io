fd0579520e0d0c036138bd69b5f1fb95657360c1
Jeff Pei <ypei@ebay.com>
2015-11-09 15:07:13 -0800

    update

--- docs/custom.js
+++ docs/custom.js
@@ -5,3 +5,23 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 
 ga('create', 'UA-23260647-5', 'auto');
 ga('send', 'pageview');
+
+
+
+<!-- Start of StatCounter Code for Default Guide -->
+<script type="text/javascript">
+var sc_project=10678959; 
+var sc_invisible=1; 
+var sc_security="957d7138"; 
+var scJsHost = (("https:" == document.location.protocol) ?
+"https://secure." : "http://www.");
+document.write("<sc"+"ript type='text/javascript' src='" +
+scJsHost+
+"statcounter.com/counter/counter.js'></"+"script>");
+</script>
+<noscript><div class="statcounter"><a title="shopify
+analytics" href="http://statcounter.com/shopify/"
+target="_blank"><img class="statcounter"
+src="http://c.statcounter.com/10678959/0/957d7138/0/"
+alt="shopify analytics"></a></div></noscript>
+<!-- End of StatCounter Code for Default Guide -->
