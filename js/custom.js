088bd91d5218108e6203108696f2b82005766ef6
Jeff Pei <ypei@ebay.com>
2015-11-15 14:58:39 -0800

    update

--- js/custom.js
+++ js/custom.js
@@ -15,29 +15,56 @@
 			if ($(".graph.bar").length>0) {
 				// Data for bar charts
 				
-			var barChartData = {
-			    labels: ["Ping 1500 Servers"],
-			    datasets: [
-			        {
-			            label: "Parallec",
-			            fillColor: "rgba(151,187,205,0.5)",
-			            strokeColor: "rgba(151,187,205,0.8)",
-			            highlightFill: "rgba(151,187,205,0.75)",
-			            highlightStroke: "rgba(151,187,205,1)",
-			            data: [2.2]
-			        },
-			        {
-			            label: "FPing",
-			            fillColor: "rgba(220,220,220,0.5)",
-			            strokeColor: "rgba(220,220,220,0.8)",
-			            highlightFill: "rgba(220,220,220,0.75)",
-			            highlightStroke: "rgba(220,220,220,1)",		
-			            data: [4.5]
-			        }
-			  	  ]
-				};
-				
+				var barChartData = {
+				    labels: ["Ping 1500 Servers"],
+				    datasets: [
+				        {
+				            label: "Parallec",
+				            fillColor: "rgba(151,187,205,0.5)",
+				            strokeColor: "rgba(151,187,205,0.8)",
+				            highlightFill: "rgba(151,187,205,0.75)",
+				            highlightStroke: "rgba(151,187,205,1)",
+				            data: [2.2]
+				        },
+				        {
+				            label: "FPing",
+				            fillColor: "rgba(220,220,220,0.5)",
+				            strokeColor: "rgba(220,220,220,0.8)",
+				            highlightFill: "rgba(220,220,220,0.75)",
+				            highlightStroke: "rgba(220,220,220,1)",		
+				            data: [4.5]
+				        }
+				  	  ]
+					};
+			var barChartDataHttp = {
+				    labels: ["HTTP on 8K Servers"],
+				    datasets: [
+				        {
+				            label: "To Mem",
+				            fillColor: "rgba(151,187,205,0.5)",
+				            strokeColor: "rgba(151,187,205,0.8)",
+				            highlightFill: "rgba(151,187,205,0.75)",
+				            highlightStroke: "rgba(151,187,205,1)",
+				            data: [12.0]
+				        },
+				        {
+				            label: "To E.S.",
+				            fillColor: "rgba(220,220,220,0.5)",
+				            strokeColor: "rgba(220,220,220,0.8)",
+				            highlightFill: "rgba(220,220,220,0.75)",
+				            highlightStroke: "rgba(220,220,220,1)",		
+				            data: [15.6]
+				        }
+				  	  ]
+					};				
 				// Bar Charts Initialization		
+				$(window).load(function() {
+					var ctx = document.getElementById("http-bars").getContext("2d");
+					window.myBar = new Chart(ctx).Bar(barChartDataHttp, {
+						responsive : true,
+						multiTooltipTemplate: "<%= datasetLabel %> - <%= value %> Sec"
+					});
+				});				
 				$(window).load(function() {
 					var ctx = document.getElementById("fping-bars").getContext("2d");
 					window.myBar = new Chart(ctx).Bar(barChartData, {
